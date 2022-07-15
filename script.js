0083e2bda7d0dc001239535860adc174ecc35721
kelsie kidd <kelsiekidd93@gmail.com>
2022-07-14 23:04:11 -0500

    save to local storage

--- script.js
+++ script.js
@@ -1,67 +1,37 @@
- // current day date applied to the header of the index.html sheet.
- var currentDayEl = document.querySelector("#currentDay");
-var saveButton = [];
 
- setInterval(function(){
-     currentDayEl.textContent = moment().format("dddd MMM Do YYYY");
- });
+let currentHour = moment().format('H'); 
+document.querySelector("#currentDay").textContent = 
+    moment().format("dddd MMM Do YYYY");
 
- let saveToLocalStorage = 
+const hours = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
 
- 
+hours.forEach((hour,i) => {
+    
+    let thisHour = i+9;
+    document.querySelector('.container').innerHTML +=
+        `<div class="time-block row">
+            <div class="hour col-6 col-md-1">${hour}</div>
+            <textarea class="${thisHour<currentHour ? 'past' : thisHour == currentHour ? 'present' : 'future'} description col-6 col-md-10"></textarea>
+            <div class="col-6 col-md-1">
+                <button class="saveBtn" onclick="saveStorage()">save</button>
+            </div>
+        </div>`
+});
 
-function saveButton(event){
-    const saveToLocalStorage = () => {
-localStorage.setItem("saveButton", JSON.stringify());
-console.log("saveButton");
-document.getElementById("saveButton").addEventListener("click", saveButton);
-event.preventDefault;
-    }
-}
-saveToLocalStorage = localStorage.getItem("saveButton");
+let store = eval(localStorage.hours) || [];
 
+const displayStorage = () => {
+    store.forEach((item,i) => {
+        $('textarea').eq(i).val(item);
+    });
+};
 
+displayStorage();
 
-
-
-// let descriptionText = document.querySelector(".description");
-
-
-
-// function pick(){
-//     localStorage.setItem("description", descriptionText.value);
-//     console.log(descriptionText)
-//     document.getElementById("description").addEventListener("click", pick)
-// }
-//     descriptionText.value = localStorage.getItem("description");
-
-
-// document.getElementById("saveBtn").addEventListener("click", function() {
-//     const addMessage = (event)=> {
-//     event.preventDefault();
-//     let Message = {
-//         Id: saveBtn.now(),
-//         saveBtn: document.getElementById("saveBtn").value;
-//     }
-//     Messages.push(Message);
-//     document.querySelector("form").reset;
-//     console.log("saveBtn",saveBtn);
-//     }
-// });
-
-// const storageInput = document.querySelector(".storage");
-// const text = document.querySelector(".Text");
-// const saveBtn = document.querySelector("#saveBtn");
-
-// let textContent = "";
-
-
-// storageInput.addEventListener("input", letter => {
-
-//     text.innerHTML = letter.target.value
-// })
-
-// const saveToLocalStorage = () => {
-//     localStorage.setItem("textInput", text.textContent)
-// }
-// saveBtn.addEventListener("click", saveToLocalStorage)
+const saveStorage = () => {
+    store = [];
+    hours.forEach((hour,i) => {
+        store.push($('textarea').eq(i).val())
+    });
+    localStorage.hours = JSON.stringify(store);
+};
